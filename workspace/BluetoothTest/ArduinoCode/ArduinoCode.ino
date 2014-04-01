0009ff05b4541aaab4f4fb374c18f646a7323a04
Aaryn Glosterm <agg1991@yahoo.com>
2014-03-31 21:23:16 -0400

    One motor working in Arduino Code

--- workspace/BluetoothTest/ArduinoCode/ArduinoCode.ino
+++ workspace/BluetoothTest/ArduinoCode/ArduinoCode.ino
@@ -1,33 +1,25 @@
 #include <SoftwareSerial.h>
 
-int motor1Pin1 = 3; // pin 2 on L293D IC
-int motor1Pin2 = 4; // pin 7 on L293D IC
-int enable1Pin = 6; // pin 1 on L293D IC
-int motor2Pin1 = 8; // pin 10 on L293D IC
-int motor2Pin2 = 9; // pin 15 on L293D IC
-int enable2Pin = 11; // pin 9 on L293D IC
+int enable1Pin = 11;
+int in1Pin = 10;
+int in2Pin = 9;
+
+
 int state;
 int flag=0;        //makes sure that the serial only prints once the state
 int stateStop=0;
 const int ledPin = 13;
 
-const int motorpin = 10;
-
 SoftwareSerial mySerial(0, 1); //RX TX
 
 void setup() {
     // sets the pins as outputs:
-    //pinMode(motor1Pin1, OUTPUT);
-    //pinMode(motor1Pin2, OUTPUT);
-    //pinMode(enable1Pin, OUTPUT);
-    //pinMode(motor2Pin1, OUTPUT);
-    //pinMode(motor2Pin2, OUTPUT);
-    //pinMode(enable2Pin, OUTPUT);
+    pinMode(in1Pin, OUTPUT);
+    pinMode(in2Pin, OUTPUT);
+    pinMode(enable1Pin, OUTPUT);
+   
     // sets enable1Pin and enable2Pin high so that motor can turn on:
-    //digitalWrite(enable1Pin, HIGH);
-    //digitalWrite(enable2Pin, HIGH);
-    pinMode(motorpin, OUTPUT);
-    pinMode(ledPin, OUTPUT);
+    digitalWrite(enable1Pin, HIGH);
     // initialize serial communication at 9600 bits per second:
     Serial.begin(9600);
     mySerial.begin(9600);
@@ -41,82 +33,35 @@ void loop() {
     }
     // if the state is '1' the DC motor will go forward
     if (state == '1') {
-        //digitalWrite(motor1Pin1, HIGH);
-        //digitalWrite(motor1Pin2, LOW); 
-        //digitalWrite(motor2Pin1, LOW);
-        //digitalWrite(motor2Pin2, HIGH);
-        digitalWrite(ledPin, HIGH);
+        digitalWrite(in1Pin, HIGH);
+        digitalWrite(in2Pin, LOW); 
+        
         if(flag == 0){
           Serial.println("Go Forward!");
           flag=1;
         }
+
     }
     
-    // if the state is '2' the motor will turn left
     else if (state == '2') {
-        /*digitalWrite(motor1Pin1, HIGH); 
-        digitalWrite(motor1Pin2, LOW); 
-        digitalWrite(motor2Pin1, LOW);
-        digitalWrite(motor2Pin2, LOW);
-        */
-        digitalWrite(ledPin, LOW);
+        digitalWrite(in1Pin, LOW);
+        digitalWrite(in2Pin, HIGH);
+        
         if(flag == 0){
-          Serial.println("Turn LEFT");
+          Serial.println("GO BACK");
           flag=1;
         }
-        //delay(1500);
-        //state=3;
-        //stateStop=1;
     }
+    
     // if the state is '3' the motor will Stop
-    else if (state == '3' || stateStop == 1) {
-        /*digitalWrite(motor1Pin1, LOW); 
-        digitalWrite(motor1Pin2, LOW); 
-        digitalWrite(motor2Pin1, LOW);
-        digitalWrite(motor2Pin2, LOW);
-        */
-
-        digitalWrite(motorpin, HIGH);
+    else if (state == '3') {
+        digitalWrite(in1Pin, LOW);
+        digitalWrite(in2Pin, LOW);
         if(flag == 0){
           Serial.println("STOP!");
           flag=1;
         }
         stateStop=0;
-    }
-    // if the state is '4' the motor will turn right
-    else if (state == '4') {
-        /*digitalWrite(motor1Pin1, LOW); 
-        digitalWrite(motor1Pin2, LOW); 
-        digitalWrite(motor2Pin1, LOW);
-        digitalWrite(motor2Pin2, HIGH);
-        */
-        if(flag == 0){
-          Serial.println("Turn RIGHT");
-          flag=1;
-        }
-        delay(1500);
-        state=3;
-        stateStop=1;
-    }
-    // if the state is '5' the motor will Reverse
-    else if (state == '5') {
-        
-        /*digitalWrite(motor1Pin1, LOW); 
-        digitalWrite(motor1Pin2, HIGH);
-        digitalWrite(motor2Pin1, HIGH);
-        digitalWrite(motor2Pin2, LOW);
-        */
-        if(flag == 0){
-          Serial.println("Reverse!");
-          flag=1;
-        }
-    }
-    else if (state == '8') {
-       digitalWrite(motorpin, HIGH);
-       //analogWrite(motorpin, 255);
-       //digitalWrite(motorpin, HIGH);
-       Serial.println("Yo the motor should be on");
-       delay(5000); 
     }
     //For debugging purpose
     //Serial.println(state);
